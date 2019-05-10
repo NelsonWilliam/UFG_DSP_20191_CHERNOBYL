57b5376f715ca3f13973d7a8748b1fc558608d5a
larissachyevena <larissac.lopes@hotmail.com>
2019-05-10 14:30:37 -0300

    Implementação inicial do front-end UPDATE

--- Sistema/src/main/resources/static/js/filme.js
+++ Sistema/src/main/resources/static/js/filme.js
@@ -1,126 +1,37 @@
-var commentData = [{
+$(function() {
+    var inDexValue;
 
-        author: "Cot",
-        text: "Miau"
-
-    },
-
-    {
-        author: "Dag",
-        text: "Auau"
-    }
-];
-
-
-var CommentBox = React.createClass({
-    displayName: "CommentBox",
-    getInitialState: function() {
-        return {
-            data: commentData
-        };
-
-    },
-    handleCommentSubmit: function(comment) {
-        this.props.data.push(comment);
-        var comments = this.state.data;
-        var newComments = comments.concat([comment]);
-        this.setState({ data: newComments });
-    },
-    render: function() {
-        return (
-            React.createElement("div", { className: "comment-box" },
-                React.createElement(CommentForm, { data: this.props.data, onCommentSubmit: this.handleCommentSubmit }),
-                React.createElement(CommentList, { data: this.props.data })));
-
-
-    }
-});
-
-var CommentList = React.createClass({
-    displayName: "CommentList",
-    render: function() {
-        return (
-            React.createElement("div", { className: "comment-list" },
-                this.props.data.map(function(c) {
-                    return (
-                        React.createElement(Comment, { author: c.author, text: c.text }));
-
-                })));
-
-
-    }
-});
-
-var CommentForm = React.createClass({
-    displayName: "CommentForm",
-    handleSubmit: function(e) {
-        e.preventDefault();
-        var authorVal = e.target[0].value.trim();
-        var textVal = e.target[1].value.trim();
-        if (!textVal || !authorVal) {
-            return;
+    $('button').click(function() {
+        if ($('#userCmnt').val().length == '') {
+            alert('Please Enter Your Comment');
+            return true;
         }
-        this.props.onCommentSubmit({ author: authorVal, text: textVal });
-        e.target[0].value = '';
-        e.target[1].value = '';
-        return;
-    },
-    render: function() {
-        return (
-            React.createElement("form", { className: "comment-form form-group", onSubmit: this.handleSubmit },
-                React.createElement("div", { className: "input-group" },
-                    React.createElement("span", { className: "input-group-addon" }, "Nome"),
-                    React.createElement("input", { type: "text", placeholder: "Your name", className: "form-control" })),
+        var userCmnt = $('#userCmnt').val();
+        if ($('#submit').hasClass('editNow')) {
 
-                React.createElement("div", { className: "input-group" },
-                    React.createElement("span", { className: "input-group-addon" }, "Resenha"),
-                    React.createElement("input", { type: "text", placeholder: "Say something...", className: "form-control" })),
+            $('#cmntContr>div.viewCmnt').eq(inDexValue).children('p').html(userCmnt);
 
-                React.createElement("input", { type: "button", value: "Postar", className: "btn btn-primary" })));
+        } else {
 
+            $('#cmntContr').append("<div class='viewCmnt'><p>" + userCmnt + "</p><span class='edit'></span><span class='delete'></span></div>");
+        }
+        $('#userCmnt').val('');
+        $(this).removeClass('editNow');
+    });
 
-    }
+    // Delete 
+    $('#cmntContr').on('click', '.delete', function() {
+        confirm("Delete Coformation");
+        $(this).parent().remove();
+    });
+    // Edit
+    $('#cmntContr').on('click', '.edit', function() {
+
+        var toEdit = $(this).prev('p').html();
+        //alert(toEdit);
+        $('#userCmnt').val(toEdit);
+        $('button').addClass('editNow');
+        inDexValue = $(this).parent('div.viewCmnt').index();
+
+    });
 });
-
-var Comment = React.createClass({
-    displayName: "Comment",
-    render: function() {
-        return (
-            React.createElement("div", { className: "comment" },
-                React.createElement("h2", { className: "author" }, this.props.author),
-                this.props.text));
-
-
-    }
-});
-
-React.render(
-    React.createElement(CommentBox, { data: commentData }),
-    document.getElementById('app'));
-
-$(document).ready(
-    function() {
-        $('#button').click(
-            function() {
-                var toAdd = $('input[name=ListItem]').val();
-                $('ol').append('<li>' + toAdd + '</li>');
-            });
-
-        $("input[name=ListItem]").keyup(function(event) {
-            if (event.keyCode == 13) {
-                $("#button").click();
-            }
-        });
-
-        $(document).on('dblclick', 'li', function() {
-            $(this).toggleClass('strike').fadeOut('slow');
-        });
-
-        $('input').focus(function() {
-            $(this).val('');
-        });
-
-        $('ol').sortable();
-
-    }
-);
